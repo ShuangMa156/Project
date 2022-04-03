2baed1298d84f13c0a603cd8fbbe28359c0b4760
ShuangMa156 <91810526+ShuangMa156@users.noreply.github.com>
2022-04-03 22:58:50 +0800

    Create killall.sh
    在停止程序运行的脚本文件中加入对ftp文件下载进程的kill操作

---
+++ week6/project/killall.sh
@@ -0,0 +1,13 @@
+###########################################################
+# 停止数据中心后台服务程序的脚本
+###########################################################
+
+#终止调度程序
+killall -9 procctl
+#通知服务程序退出
+killall gzipfiles crtsurfdata deletefiles ftpgetfiles
+
+sleep 3
+
+#让全部的服务程序强制退出
+killall -9 gzipfiles crtsurfdata deletefiles ftpgetfiles
