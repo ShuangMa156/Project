ea6399205f3bdc5d8f745e7a10f58bbc5fc808ca
ShuangMa156 <91810526+ShuangMa156@users.noreply.github.com>
2022-04-04 12:05:30 +0800

    Update killall.sh

--- week6/project/killall.sh
+++ week6/project/killall.sh
@@ -5,9 +5,9 @@
 #终止调度程序
 killall -9 procctl
 #通知服务程序退出
-killall gzipfiles crtsurfdata deletefiles ftpgetfiles
+killall gzipfiles crtsurfdata deletefiles ftpgetfiles ftpputfiles
 
 sleep 3
 
 #让全部的服务程序强制退出
-killall -9 gzipfiles crtsurfdata deletefiles ftpgetfiles
+killall -9 gzipfiles crtsurfdata deletefiles ftpgetfiles ftpputfiles
